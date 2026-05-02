6be93ed585af21df00401dedca0f0f5eff502d65
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-02 16:17:02 +0300

    feat: added auth service and controller for it and user service

---
+++ src/users/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from '../create-user.dto';
+import { PasswordService } from '../password/password.service';
+import { User } from '../user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly passwordService: PasswordService,
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+  public async findOneByEmail(email: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { email } });
+  }
+  public async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await this.passwordService.hash(
+      createUserDto.password,
+    );
+    return await this.usersRepository.save({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+  }
+  public async findOne(id: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { id } });
+  }
+}
