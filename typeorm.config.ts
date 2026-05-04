72922d96bdf8a0fadd7a87389e4c6fe204a86d8c
andriibabiuk <andrii.babiuk.dev@gmail.com>
2026-05-04 18:15:27 +0300

    feat: add TypeORM config and generate initial migration

---
+++ typeorm.config.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+import { DataSource } from 'typeorm';
+config();
+
+export default new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST ?? 'localhost',
+  port: parseInt(process.env.DB_PORT ?? '5433'),
+  username: process.env.DB_USERNAME ?? 'postgres',
+  password: process.env.DB_PASSWORD ?? 'postgres',
+  database: process.env.DB_NAME ?? 'tasks',
+  synchronize: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/src/migrations/*{.ts,.js}'],
+});
